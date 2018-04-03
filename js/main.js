2fbe20e9b24e2b067c297ab4e82dd3897bc62c3b
Vitaly39 <vgnatyk39@gmail.com>
2018-04-03 14:24:35 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -2,16 +2,27 @@
 
 var FORM = document.querySelector('.search-hotels__form');
 var SHOW_FORM_BUTTON = document.querySelector('.open-form');
-var LOGIN_FORM = document.querySelector('[name=arrival__date]')
+var ARRIVAL_DATE = document.querySelector('[name=arrival__date]');
+var DEPARTURE_DATE = document.querySelector('[name=departure__date]');
+var ADULTS_COUNT = document.querySelector('[name=adults__amount]');
+var KIDS_COUNT = document.querySelector('[name=children__amount]');
 
 SHOW_FORM_BUTTON.addEventListener('click', function (evt) {
   evt.preventDefault();
   if (FORM.classList.contains('visually-hidden')) {
-    LOGIN_FORM.focus();
+    ARRIVAL_DATE.focus();
     FORM.classList.remove('visually-hidden')
     FORM.classList.add('form-bounce');
   } else {
     FORM.classList.add('visually-hidden')
     FORM.classList.remove('form-bounce');
+  }
+});
+
+FORM.addEventListener('submit', function (evt) {
+  if (!ARRIVAL_DATE.value || !DEPARTURE_DATE.value || !ADULTS_COUNT.value || !KIDS_COUNT.vale) {
+    evt.preventDefault();
+    FORM.classList.remove('form-error');
+    FORM.classList.add('form-error');
   }
 });
